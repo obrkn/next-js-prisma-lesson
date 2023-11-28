d0d45f2182c904a671cb15a9eaca41c7ad1b2fce
obrkn <k33mdukb9n@yahoo.co.jp>
2023-11-28 15:58:18 -0800

    commit 2

--- src/app/components/PostsList.tsx
+++ src/app/components/PostsList.tsx
@@ -1,15 +1,37 @@
 import React from "react";
 import prisma from "@/db";
 import PostsListItem from "./PostsListItem";
+import { revalidatePath } from "next/cache";
 
 const PostsList = async () => {
   const posts = await prisma.post.findMany();
   console.log("Data", posts);
 
+  const onDelete = async (id: string) => {
+    "use server";
+    await prisma.post.delete({ where: { id } });
+    revalidatePath("/");
+  };
+
+  const onPublish = async (id: string, published: boolean) => {
+    "use server";
+
+    await prisma.post.update({
+      where: { id },
+      data: { published },
+    });
+    revalidatePath("/");
+  };
+
   return (
     <div>
       {posts.map((post) => (
-        <PostsListItem key={post.id} {...post} />
+        <PostsListItem
+          key={post.id}
+          {...post}
+          onDelete={onDelete}
+          onPublish={onPublish}
+        />
       ))}
     </div>
   );
--- src/app/components/PostsListItem.tsx
+++ src/app/components/PostsListItem.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
 
@@ -6,9 +8,18 @@ type PostItemProps = {
   title: string;
   content: string;
   published: boolean;
+  onDelete: (id: string) => void;
+  onPublish: (id: string, published: boolean) => void;
 };
 
-const PostsListItem = ({ id, title, content, published }: PostItemProps) => {
+const PostsListItem = ({
+  id,
+  title,
+  content,
+  published,
+  onDelete,
+  onPublish,
+}: PostItemProps) => {
   return (
     <div className="flex items-center gap-2 my-3">
       <div>
@@ -17,12 +28,24 @@ const PostsListItem = ({ id, title, content, published }: PostItemProps) => {
         <div>{published ? "Published" : "Not Published"}</div>
       </div>
       <div>
+        <button
+          onClick={() => onPublish(id, !published)}
+          className="border border-zinc-900 text-zinc-900 px-2 py-1 text-center"
+        >
+          {published ? "Unpublish" : "Publish"}
+        </button>
         <Link
           href={`/view/${id}`}
           className="border border-zinc-900 text-zinc-900 px-2 py-1 text-center"
         >
           View
         </Link>
+        <button
+          onClick={() => onDelete(id)}
+          className="border border-zinc-900 text-white bg-red-500 px-2 py-1 text-center"
+        >
+          DELETE
+        </button>
       </div>
     </div>
   );
